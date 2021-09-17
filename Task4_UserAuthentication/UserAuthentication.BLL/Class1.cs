c3bd4de11daeb6c33c14a5e9a8f4d509481d69e1
Alex <alexei.truhan@gmail.com>
2021-09-17 18:56:09 +0300

    init commit

---
+++ Task4_UserAuthentication/UserAuthentication.BLL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace UserAuthentication.BLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ Task4_UserAuthentication/UserAuthentication.DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace UserAuthentication.DAL
+{
+    public class Class1
+    {
+    }
+}
